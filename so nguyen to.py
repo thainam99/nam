21119ce44dd0a29d114483bfd23d5f464c398ae1
thainam99 <48400628+thainam99@users.noreply.github.com>
2019-03-17 20:14:26 +0700

    Add files via upload

---
+++ so nguyen to.py
@@ -0,0 +1,11 @@
+﻿n = int(input('nhap n='))
+if n == 1:
+    print("không là số nguyên tố")
+else:
+        i = 2
+        while n % i != 0:
+            i = i + 1
+        if n == i:
+            print("là số nguyên tố")
+        else:
+            print("không là số ngyên tố")
